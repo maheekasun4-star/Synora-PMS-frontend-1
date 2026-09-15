d007f7ea4e08004b4e29741776759b1042620228
maheekasun4-star <maheekasun4@gmail.com>
2026-09-15 12:59:23 +0530

    Update production API URL in index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
  // baseURL: 'http://localhost:5000/api',
-  baseURL: 'https://synora-bk-production-bac0.up.railway.app/api', // Replace with your production API URL
+  baseURL: 'https://synora-bk-production-71ef.up.railway.app/api', // Replace with your production API URL
 });
 
 api.interceptors.request.use((config) => {
